fb989d4f74cf140afa60f88a8183a83bf48eee2b
Markus Kreth <markus.kreth@innowake.de>
2017-10-06 21:48:08 +0100

    Location are displayed. MapView not working.

--- app/src/main/java/de/kreth/parkinghelper/MainActivity.java
+++ app/src/main/java/de/kreth/parkinghelper/MainActivity.java
@@ -1,6 +1,8 @@
 package de.kreth.parkinghelper;
 
+import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +15,13 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -107,22 +116,44 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutHolder holder;
             if(convertView == null) {
-                convertView = getLayoutInflater().inflate(R.layout.position_item_layout, parent);
-                holder = new LayoutHolder();
-                holder.caption = convertView.findViewById(R.id.textViewCaption);
-                holder.description = convertView.findViewById(R.id.textViewDescription);
+                convertView = getLayoutInflater().inflate(R.layout.position_item_layout, null);
+                holder = new LayoutHolder(convertView);
                 convertView.setTag(holder);
             } else {
                 holder = (LayoutHolder) convertView.getTag();
             }
-            holder.update(getItem(position));
+            final PositionItem item = getItem(position);
+            holder.update(item);
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    StringBuilder geo = new StringBuilder("geo:");
+                    geo.append(item.getLocation().getLatitude());
+                    geo.append(',');
+                    geo.append(item.getLocation().getLongitude());
+                    Uri gmmIntentUri = Uri.parse(geo.toString());
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(mapIntent);
+                    }
+                }
+            });
             return convertView;
         }
 
         private class LayoutHolder {
-            TextView caption;
-            TextView description;
-            void update(PositionItem item) {
+            final TextView caption;
+            final TextView description;
+            final MapView map;
+
+            public LayoutHolder(View convertView) {
+                caption = convertView.findViewById(R.id.textViewCaption);
+                description = convertView.findViewById(R.id.textViewDescription);
+                map = convertView.findViewById(R.id.mapView);
+            }
+
+            void update(final PositionItem item) {
                 caption.setText(item.getName());
                 StringBuilder desc = new StringBuilder();
                 desc
@@ -130,6 +161,17 @@ public class MainActivity extends AppCompatActivity {
                         .append("\n")
                         .append(item.getLocation().getLatitude());
                 description.setText(desc);
+                OnMapReadyCallback mapReadyCallback = new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                        Location loc = item.getLocation();
+                        googleMap
+                                .addMarker(new MarkerOptions()
+                                        .position(new LatLng(loc.getLatitude(), loc.getLongitude())).title(item.getName()));
+                    }
+                };
+                map.getMapAsync(mapReadyCallback);
             }
         }
     }
